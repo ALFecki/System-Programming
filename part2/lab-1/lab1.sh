2bc4e345bf1be01138722263c75f9b9f48f2cc40
Skvortsov A.V <alexskvr03@gmail.com>
2024-02-03 14:52:58 +0300

    checksum added

--- part2/lab-1/lab1.sh
+++ part2/lab-1/lab1.sh
@@ -1,27 +1,78 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-  echo "Please, enter all parameters $0 <dir_path> <regex>"
+usage() {
+  echo "Usage: $0 [-r <regex>|-n <filename>] <dir_path>"
   exit 1
+}
+
+if [ $# -lt 2 ]; then
+  usage
 fi
 
-path=$1
-pattern=$2
+pattern=""
+filename=""
+path=""
+
+while getopts ":r:n:" opt; do
+  case $opt in
+    r)
+      pattern="$OPTARG"
+      ;;
+    n)
+      filename="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+path="$1"
+
+if [ -z "$pattern" ] && [ -z "$filename" ]; then
+  echo "Please specify either a regex pattern (-r) or a filename (-n)."
+  usage
+fi
 
 if [ ! -d "$path" ]; then
   echo "No such file or directory."
   exit 1
 fi
 
+if [ -e "output.txt" ]; then
+  > output.txt
+fi
+
+calculate_checksum() {
+    local file="$1"
+    local sum=0
+
+    while IFS= read -r -n1 byte; do
+        byte_value=$(printf "%d" "'$byte")
+        ((sum += byte_value))
+    done < "$file"
+
+    echo "$sum"
+}
 
 traverse_directory() {
   local dir="$1"
   for file in "$dir"/*; do
     if [ -f "$file" ]; then
-
-      if [[ $(basename "$file") =~ $pattern ]]; then
-        echo "$file founded"
-        echo "$(basename "$file")" >> output.txt
+      if [ -n "$pattern" ]; then
+        if [[ $(basename "$file") =~ $pattern ]]; then
+          checksum=$(calculate_checksum "$file")
+          echo "$file founded, checksum: $checksum"
+          echo "$(basename "$file, checksum: $checksum")" >> output.txt
+        fi
+      elif [ -n "$filename" ]; then
+        if [ "$(basename "$file")" = "$filename" ]; then
+          checksum=$(calculate_checksum "$file")
+          echo "$file founded, checksum: $checksum"
+          echo "$(basename "$file, checksum: $checksum")" >> output.txt
+        fi
       fi
     elif [ -d "$file" ]; then
       # if [ "$file" != "$dir" ]; then
@@ -31,9 +82,7 @@ traverse_directory() {
   done
 }
 
-if [ -e "output.txt" ]; then
-  > output.txt
-fi
+
 
 traverse_directory "$path"
 
